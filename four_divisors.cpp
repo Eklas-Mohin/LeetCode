933bd0039765e3198ac247869142a7b5589b52d8
Eklas-Mohin <mohin030299@gmail.com>
2024-10-12 11:57:16 +0600

    LeetCode 1390. Four Divisors

--- four_divisors.cpp
+++ four_divisors.cpp
@@ -1,8 +1,10 @@
-/**
- *    author: mohin    
-**/
+/** **************************
+ *   author:  mohin          *
+ *   problem: LeetCode 1390  *
+ *   verdict: Accepted       *
+ *****************************/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -10,21 +12,22 @@ class Solution {
 public:
     int sumOfDivisors(int n) {
         int count{1}, ans{1};
-        for (int i = 2; i <= (sqrt(n)); ++i) {
+        for (int i = 2; i <= sqrt(n); ++i) {
             if (n % i == 0) {
                 count = 1;
                 while (n % i == 0) {
                     count += 1;
                     n /= i;
                 }
-                ans = ans * ((pow(i, count) - 1) / (i - 1));
+                ans *= (pow(i, count) - 1) / (i - 1);
             }
         }
         if (n != 1) {
-            ans = ans * ((pow(n, 2) - 1) / (n - 1));
+            ans *= (pow(n, 2) - 1) / (n - 1);
         }
         return ans;
     }
+
     int numOfDivisor(int n) {
         int temp{n}, count{};
         vector<int> v;
@@ -35,7 +38,7 @@ public:
         if (count) {
             v.push_back(count + 1);
         }
-        for (int i = 3; i <= (sqrt(temp)); i += 2) {
+        for (int i = 3; i <= sqrt(temp); i += 2) {
             if (temp % i == 0) {
                 count = 0;
                 while (temp % i == 0) {
@@ -50,10 +53,11 @@ public:
             v.push_back(2);
         }
         for (int i = 0; i < v.size(); ++i) {
-            count = count * v[i];
+            count *= v[i];
         }
         return count;
     }
+
     int sumFourDivisors(vector<int>& nums) {
         int ans{};
         for (int i = 0; i < nums.size(); ++i) {
@@ -66,7 +70,6 @@ public:
     }
 };
 
-// Driver code
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
