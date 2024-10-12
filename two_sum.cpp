8dbd2daec2c9a3ff7234252ebeceaa13f0ccea45
Eklas-Mohin <mohin030299@gmail.com>
2024-10-13 01:41:39 +0600

    LeetCode 1. Two Sum

--- two_sum.cpp
+++ two_sum.cpp
@@ -11,45 +11,38 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
+        vector<int> result;
         unordered_map<int, int> pos;
-
         for (int i = 0; i < nums.size(); ++i) {
             int k = target - nums[i];
             if (pos.find(k) != pos.end()) { 
-                ans.push_back(pos[k]);       
-                ans.push_back(i);             
-                return ans;                   
+                result.push_back(pos[k]);       
+                result.push_back(i);             
+                return result;                   
             }
             pos[nums[i]] = i;
         }
-        
-        return ans;
+        return result;
     }
 };
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(0);
-
+    cin.tie(nullptr);cout.tie(nullptr);
     int n, x, target;
     vector<int> nums;
-
     cin >> n;
     for (int i = 0; i < n; ++i) {
         cin >> x;
         nums.push_back(x);
     }
     cin >> target;
-
     Solution obj;
-    vector<int> ans = obj.twoSum(nums, target);
-    
-    if (ans.size() == 2) {
-        printf("[%d,%d]\n", ans[0], ans[1]);
+    vector<int> result = obj.twoSum(nums, target);
+    if (result.size() == 2) {
+        printf("[%d,%d]\n", result[0], result[1]);
     } else {
         printf("No two sum solution found\n");
     }
-
     return 0;
 }
