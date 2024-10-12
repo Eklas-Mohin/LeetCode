04e888b3aec01e2e8a737cba79a6ec4fd8a86677
Eklas-Mohin <mohin030299@gmail.com>
2024-10-12 11:55:19 +0600

    LeetCode 204. Count Primes

--- count_primes.cpp
+++ count_primes.cpp
@@ -1,48 +1,51 @@
-/**
- *    author: mohin    
-**/
+/** **************************
+ *   author:  mohin          *
+ *   problem: LeetCode 204   *
+ *   verdict: Accepted       *
+ *****************************/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
     int countPrimes(int n) {
-        bool sieve[5000001];
-        memset(sieve, true, sizeof(sieve));
-        int prime_count{};
+        if (n <= 2) return 0;
+
+        vector<bool> sieve(n, true);
         sieve[0] = sieve[1] = false;
-        for (int i = 4; i <= n; i += 2) {
-            sieve[i] = false;
-        }
-        for (int i = 3; i <= sqrt(n + 1); i += 2) {
+
+        for (int i = 2; i * i < n; ++i) {
             if (sieve[i]) {
-                for (int j = i * 2; j <= n; j += i) {
+                for (int j = i * i; j < n; j += i) {
                     sieve[j] = false;
                 }
             }
         }
-        if (n > 2) {
-            prime_count = 1;
-        }
-        for (int i = 3; i < n; i += 2) {
+
+        int prime_count = 0;
+        for (int i = 2; i < n; ++i) {
             if (sieve[i]) {
-                prime_count += 1;
+                prime_count++;
             }
         }
+
         return prime_count;
     }
 };
 
-// Driver code
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    int n{}, prime_count{};
+
+    int n;
     cin >> n;
+
     Solution obj;
-    prime_count = obj.countPrimes(n);
+    int prime_count = obj.countPrimes(n);
+    
     cout << prime_count << endl;
+
     return 0;
 }
